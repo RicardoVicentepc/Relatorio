6589d08f57fe219aee69cbb71596614f07d9ac47
Ricardo Vicente <86174349+RicardoVicentepc@users.noreply.github.com>
2022-11-09 17:32:05 -0300

    DEU TEMPO MISERA FALTO MAPS

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Button, FlatList, Text, TextInput, View, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-web';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
 
+  function HomeScreen() {
   const [dados, setDados] = useState();
   const [turma, setTurma] = useState();
   const [serie, setSerie] = useState();
@@ -11,142 +14,138 @@ export default function App() {
   const [aluno, setAluno ] = useState();
   const [comentario, setComentario] = useState();
 
-
-
-  const verificar = () => {
-    const valores = turma;
-
-
-    fetch('http://localhost:8080/ProjetoRelatorio/ProjetoRelatorio/relatorio-json-insert.php', {
-      method: 'post',
-      header: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        turma: turma,
-        serie: serie,
-        componente: componente,
-        aluno: aluno,
-        comentario: comentario,
+    const verificar = () => {
+      const valores = turma;
+  
+  
+      fetch('http://localhost:8080/ProjetoRelatorio/ProjetoRelatorio/relatorio-json-insert.php', {
+        method: 'post',
+        header: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          turma: turma,
+          serie: serie,
+          componente: componente,
+          aluno: aluno,
+          comentario: comentario,
+        })
       })
-    })
-
-    setDados(valores)
+  
+      setDados(valores)
+    }
+  
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <View style={styles.forms}>
+        <TextInput
+          placeholder='Turma'
+          placeholderTextColor={'#000'}
+          style={styles.TextInput}
+  
+          onChangeText={text => setTurma(text)}
+        />
+         <TextInput
+          placeholder='Série'
+          placeholderTextColor={'#000'}
+          style={styles.TextInput}
+  
+          onChangeText={text => setSerie(text)}
+        />
+         <TextInput
+          placeholder='Componente Curricular'
+          placeholderTextColor={'#000'}
+          style={styles.TextInput}
+  
+          onChangeText={text => setComponente(text)}
+        />
+         <TextInput
+          placeholder='Aluno'
+          placeholderTextColor={'#000'}
+          style={styles.TextInput}
+  
+          onChangeText={text => setAluno(text)}
+        />
+         <TextInput
+          placeholder='Comentarios'
+          placeholderTextColor={'#000'}
+          color="#000"
+          style={styles.Comentarios}
+          maxLength={40}
+  
+          onChangeText={text => setComentario(text)}
+        />
+        <View>
+          
+        <Button
+        title='salvar'
+        onPress={() => verificar()}
+        color="#F9B5B5"
+        />
+          
+        </View>
+        </View>
+      </View>
+    );
+  }
+  function SettingsScreen() {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+  
+    const getRelatorios = async () => {
+       try {
+        const response = await fetch('http://localhost:8080/ProjetoRelatorio/ProjetoRelatorio/relatorio-json.php');
+        const json = await response.json();
+        setData(json.relatorios);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  
+    useEffect(() => {
+      getRelatorios();
+    }, []);
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ idRelatorio }, index) => idRelatorio}
+          renderItem={({ item }) => (
+            <Text>id:{item.idRelatorio}  Aluno: {item.aluno}, Comentario:{item.comentario}</Text>
+          )}
+        />
+      )}
+    </View>
+      </View>
+    );
+  }
+  function mapa(){
+    return(     
+       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Colocar maps</Text>
+      </View>
+    )
   }
 
-  // const getCategorias = async () => {
-  //   try {
-  //     const response = await fetch('http://localhost:8080/Project/categoria-json.php');
-  //     const json = await response.json();
-  //     setData(json.categorias);
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-  // const getProducts = async () => {
-  //   try {
-  //     const responseProduct = await fetch('http://localhost:8080/Project/produto-json.php');
-  //     const json = await responseProduct.json();
-  //     setDataProd(json.produtos);
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     setProduct(false);
-  //   }
-  // }
-  // useEffect(() => {
-  //   getCategorias();
-  //   getProducts();
-  // }, []);
 
+
+  
+  const Tab = createBottomTabNavigator();
   return (
-    <ScrollView style={{backgroundColor:'#fff6f6'}}>
 
-    <View style={styles.container}>
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Relatorio" component={HomeScreen} />
+        <Tab.Screen name="Consulta"  component={SettingsScreen} />
+        <Tab.Screen name="Mapa"  component={mapa} />
 
-      {/* {isLoading ? <ActivityIndicator /> : (
-          <FlatList
-            data={data}
-            keyExtractor={({ idCategoria }, index) => idCategoria}
-            renderItem={({ item }) => (
-              <Text>{item.idCategoria}, {item.categoria}</Text>
-            )}
-          />
-        )}
-
-        {isLoadingProduct ? <ActivityIndicator /> : (
-          <FlatList
-            data={dataProduct}
-            keyExtractor={({ idProduto }, index) => idProduto}
-            renderItem={({ item }) => (
-              <Text>{item.idProduto}, {item.produto}: R${item.valor}  {item.categoria}</Text>
-            )}
-          />
-        )} */
-      }
-      <Text style={styles.h1}>Relatório do Bimestre</Text>
-      <View style={styles.forms}>
-      <TextInput
-        placeholder='Turma'
-        placeholderTextColor={'#000'}
-        autoFocus={true}
-        style={styles.TextInput}
-
-        onChangeText={text => setTurma(text)}
-      />
-       <TextInput
-        placeholder='Série'
-        placeholderTextColor={'#000'}
-        autoFocus={true}
-        style={styles.TextInput}
-
-        onChangeText={text => setSerie(text)}
-      />
-       <TextInput
-        placeholder='Componente Curricular'
-        placeholderTextColor={'#000'}
-        autoFocus={true}
-        style={styles.TextInput}
-
-        onChangeText={text => setComponente(text)}
-      />
-       <TextInput
-        placeholder='Aluno'
-        placeholderTextColor={'#000'}
-        autoFocus={true}
-        style={styles.TextInput}
-
-        onChangeText={text => setAluno(text)}
-      />
-       <TextInput
-        placeholder='Comentarios'
-        placeholderTextColor={'#000'}
-        autoFocus={true}
-        color="#000"
-        style={styles.Comentarios}
-        maxLength={40}
-
-        onChangeText={text => setComentario(text)}
-      />
-      <View>
-        
-      <Button
-      title='salvar'
-      onPress={() => verificar()}
-      color="#F9B5B5"
-      />
-        
-      </View>
-      </View>
-      
-    </View>
-    </ScrollView>
-
-
-
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
  
